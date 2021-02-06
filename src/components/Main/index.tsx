00a6b655aa80a49511d077046d5d901d5d48b160
Felipe Borges <lipex360@gmail.com>
2021-02-05 22:28:07 -0300

    Tests added

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,8 @@
+const Main = () =>
+  (
+  <div>
+    <h1>React Avançado</h1>
+  </div>
+  )
+
+export default Main
---
+++ src/components/Main/test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+
+import Main from '.'
+
+describe('<Main />', () => {
+  it('shuold be able to', () => {
+    const { container } = render(<Main />)
+
+    expect(screen.getByRole('heading', { name: /react avançado/i }))
+      .toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
